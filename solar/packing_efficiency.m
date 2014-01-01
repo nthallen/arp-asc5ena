fcd9618c04978bf21ec4a9d5ed654f8ac4d3157c
Norton Allen <allen@huarp.harvard.edu>
2014-01-01 11:56:47 -0500

    Tested simplified illumination model

--- solar/packing_efficiency.m
+++ solar/packing_efficiency.m
@@ -3,7 +3,9 @@ function area = packing_efficiency(w, h, tile_edge)
 if nargin < 3
     tile_edge = 0.127; % 5"
 end
+% corners is the area lost at the corners from cutting a 5" square
+% out of a 6" diameters circular wafer
 corners = 0.951;
 nw = floor(w/tile_edge);
 nh = floor(h/tile_edge);
-area = nw * nw * tile_edge^2;
+area = corners * nw * nh * tile_edge^2;
